6a355177b975332eda676d07cfa730065958f5ac
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-11-19 18:16:04 -0600

    phase 1 done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,17 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store'
 import { restoreCSRF, fetch } from './store/csrf'
+import * as sessionActions from './store/session';
 
 const store = configureStore()
 
 if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
 
+
   window.csrfFetch = fetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
