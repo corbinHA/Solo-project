1293df1e9e31927faeecebe0118569ea05493af0
Corbin Armendariz <corbinarmendariz@gmail.com>
2020-11-20 15:30:55 -0600

    login done

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -8,9 +8,11 @@ export async function fetch(url, options = {}) {
     options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
     options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
   }
+
   const res = await window.fetch(url, options);
 
   const contentType = (await res).headers.get('content-type');
+  
   if (contentType && contentType.includes('application/json')) {
     const data = await res.json();
     res.data = data;
